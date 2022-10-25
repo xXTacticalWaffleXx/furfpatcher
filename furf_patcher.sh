cc2b79e69535d5e27b05473fdb8b88a34eda8a9b
Luna Aphelion <luna-aphelion@proton.me>
2022-10-25 10:25:30 +0100

    added git ignore

--- furf_patcher.sh
+++ furf_patcher.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+main(){
+# create temp directory assuming it does not exist
+if ! ls temp > /dev/null; then
+  echo "temp does not exist, creating"
+  mkdir temp
+else
+  echo "temp already exists, exiting"
+  exit
+fi
+}
+
+main
